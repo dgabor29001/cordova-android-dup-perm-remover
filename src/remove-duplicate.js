66e8e81fc3b906dc088c491caaedfc8644e4c0c9
dgabor29001 <130984581+dgabor29001@users.noreply.github.com>
2024-09-19 13:53:33 +0200

    Update remove-duplicate.js
    add filter

--- src/remove-duplicate.js
+++ src/remove-duplicate.js
@@ -1,27 +1,34 @@
 
 /**
  * 
- * @param {string} content 
+ * @param {string} content
+ * @param {string[]} filter Lines to be include in the content
  * @param {string[]} exclude Permissions to to no include in the content
  * @returns 
  */
-module.exports = function(content, exclude = []){
+module.exports = function(content, filter = ['uses-permission', 'uses-feature'], exclude = []){    
     const manifestLines = content.split("\n");
     const normalized  = [];
     const added = {};
     for(let line of manifestLines){
-        let result = /android\:name\="([^"]+)"/.exec(line);
-        let permission = result && result[1];
-        if(permission){
-
-            if(exclude && exclude.indexOf(permission) !== -1){
-                continue;
+        if(filter && filter.some(f => line.includes(f))){
+            let result = /android\:name\="([^"]+)"/.exec(line);
+            let permission = result && result[1];
+            if(permission){
+                /*
+                if(filter && !filter.some(f => line.includes(f))){
+                    continue;
+                }
+                */
+                if(exclude && exclude.indexOf(permission) !== -1){
+                    continue;
+                }
+    
+                if(added[permission]){
+                    continue;
+                }
+                added[permission] = true; 
             }
-
-            if(added[permission]){
-                continue;
-            }
-            added[permission] = true; 
         }
         normalized.push(line);
     }
